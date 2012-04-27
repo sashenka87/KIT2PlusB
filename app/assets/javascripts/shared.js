5d7fc20700c8b1b1ba29d420629dc53a3e675752
Cyrus Stoller <cyrus.stoller@gmail.com>
2012-04-27 19:26:40 -0400

    Making the mm line fit in a modal

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -8,7 +8,7 @@ $(function(){
 		}
 	);
 	$(".hover_to_x").click(function(){
-		$(".hover_to_x").html("-");
+		$(".hover_to_x").html(".");
 		$(".hover_to_x").removeClass("selected_x");
 		$(this).html('<span class="red">X</span>');
 		$(this).addClass("selected_x");
